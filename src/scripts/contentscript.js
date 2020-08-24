32ec5fdf178f94516eecc1d89b390aa041bb46b2
msramalho <up201403027@fe.up.pt>
2020-08-24 11:30:22 +0100

    more info to contentscript

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -2,12 +2,14 @@ import ext from "./utils/ext";
 
 function onRequest(request, _sender, sendResponse) {
     if (request.type === 'getWorkspaces') {
-        console.log(document.querySelectorAll("a.cta_lead"));
         let workspaces = [...document.querySelectorAll("a.cta_lead")].map(el => {
+            //loginUrl only works for 24h
             return {
                 name: el.querySelector("span.link_head").textContent,
-                url: el.href,
+                loginUrl: el.href,
+                url: `https://${el.querySelector("span.cta_subdomain").textContent}`,
                 image: el.querySelector("i.team_icon").style.backgroundImage.slice(4, -1).replace(/"/g, ""),
+                lastUpdated: Date.now()
             }
         })
         sendResponse(workspaces)
